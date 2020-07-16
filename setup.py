f154320920fced5d74176cb9c4cbc67d2cc6096d
willdgn <william.digan@aphp.fr>
2020-07-16 13:02:06 +0200

    add dependecies to setup file

--- setup.py
+++ setup.py
@@ -8,12 +8,19 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+reqs = ["flashtext==2.7","Unidecode==1.1.1",
+        "json5==0.8.5","jsonschema==3.0.2",
+        "intervaltree", "pandas",
+        "psycopg2-binary"]
+
+
 setup(name='pymedext_core',
-version='0.1',
-description='pymedext core toolkit for clinical NLP',
-packages=['pymedext_core'],
-author='Antoine Neuraz, William Digan, Ivan Lerner, Alice Rogier, David Baudoin, Anita Burgun, Nicolas Garcelon, Bastien Rance ',
-author_email='william.digan@aphp.fr',
-long_description=long_description,
-long_description_content_type='text/markdown'
-,zip_safe=False)
+      version='0.1',
+      description='pymedext core toolkit for clinical NLP',
+      packages=['pymedext_core'],
+      install_requires=reqs,
+      author='Antoine Neuraz, William Digan, Ivan Lerner, Alice Rogier, David Baudoin, Anita Burgun, Nicolas Garcelon, Bastien Rance ',
+      author_email='william.digan@aphp.fr',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      zip_safe=False)
