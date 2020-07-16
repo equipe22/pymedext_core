47316d8120cc7f6f5ff7a68eefc53c623fabb674
willdgn <william.digan@aphp.fr>
2020-07-16 11:37:09 +0200

    add the setup install code

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+long_description = f.read()
+
+setup(name='pymedext_core',
+version='0.1',
+description='pymedext core toolkit for clinical NLP',
+packages=['pymedext_core'],
+author='Antoine Neuraz, William Digan, Ivan Lerner, Alice Rogier, David Baudoin, Anita Burgun, Nicolas Garcelon, Bastien Rance ',
+author_email='william.digan@aphp.fr',
+long_description=long_description,
+long_description_content_type='text/markdown'
+,zip_safe=False)
