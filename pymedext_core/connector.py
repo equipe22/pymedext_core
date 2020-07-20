9eaf61f70b7b14a229564eda1ddd2ef4dbacde38
willdgn <william.digan@aphp.fr>
2020-07-20 17:35:25 +0200

    add doc for Connector

--- pymedext_core/connector.py
+++ pymedext_core/connector.py
@@ -7,10 +7,26 @@ logger = logging.getLogger(__name__)
 
 
 class Connector:
+    """
+    TODO : make this an abstract class for other connector
+    """
     pass
 
 class DatabaseConnector:
+    """ Abstract class specialize in database connection
+    """
     def __init__(self, DB_host, DB_name, DB_port, DB_user, DB_password):
+        """initialize some basic variable to connect into a database
+
+        :param DB_host: host name
+        :param DB_name:  database name
+        :param DB_port: connection port
+        :param DB_user:  user name
+        :param DB_password: user password
+        :returns: abstract databse connector
+        :rtype: DatabaseConnect
+
+        """
         self.DB_host =  DB_host
         self.DB_name = DB_name
         self.DB_port = DB_port
@@ -18,23 +34,52 @@ class DatabaseConnector:
         self.DB_password = DB_password
         logger.info("Initialize Database Connection")
     def startConnection(self):
+        """
+        Abstract function where each DatabaseConnector should implement the
+        Database connection
+        """
         pass
 
 
 class APIConnector:
+    """ Abstact connector to an API
+    TODO : discuss with @Alice how to implement it for Doccano
+    """
     pass
 
-class cxORacleConnector:
+class cxORacleConnector(DatabaseConnector):
+    """ Abstact connector to an Oracle database using cxOracle
+    """
     pass
 
 class PostGresConnector(DatabaseConnector):
+    """
+    Abstract Connector to a Postgres Database
+    """
     def __init__(self, DB_host, DB_name, DB_port, DB_user, DB_password):
+        """Initialize a postGre connector
+
+
+        :param DB_host: host name
+        :param DB_name:  database name
+        :param DB_port: connection port
+        :param DB_user:  user name
+        :param DB_password: user password
+        :returns: A PostregesConnector
+        :rtype: POstGresConnector
+
+        """
         super().__init__( DB_host, DB_name, DB_port, DB_user, DB_password)
         logger.info("Initialize Database Connection")
         self.cur = None
         self.conn = None
         self.startConnection()
     def startConnection(self):
+        """Initialize the connection to the POstGresConnector
+        :returns: 0
+        :rtype: 0
+
+        """
         self.conn = psycopg2.connect(
             host=self.DB_host,
             database = self.DB_name,
@@ -48,6 +93,10 @@ class PostGresConnector(DatabaseConnector):
 
 
 class SSHConnector:
+    """
+    TODO: implement a connection to a server with
+    paramiko, should also extend Connector @David?
+    """
     pass
 
 
