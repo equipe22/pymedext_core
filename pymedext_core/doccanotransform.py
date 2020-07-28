144b56d83f8b20a9a943df259511304a8a81a370
7012607 <alice.rogier-ext@aphp.fr>
2020-07-28 19:07:16 +0200

    add class

--- pymedext_core/doccanotransform.py
+++ pymedext_core/doccanotransform.py
@@ -2,45 +2,47 @@ import uuid
 import json
 import os
 
-from .document import Document
+from document import Document
 
-from .doccanodocument import DoccanoDocument
+from doccanodocument import DoccanoDocument
 
-from .annotators import Annotator, Annotation
-from .doccanoannotator import DoccanoAnnotation
+from annotators import Annotator, Annotation
+from doccanoannotator import DoccanoAnnotation
 
-from .datatransform import DataTransform
+from datatransform import DataTransform
 
-class DoccanoTransform(DataTransform, Document, DoccanoDocument):
 
-    def load(DoccanoDocument):
-        return Document
+class Doccano(DataTransform):
 
-    def save(Docment, type=None, attribute= None, value=None, span=None, dict_regex_type = None, path_to_pb_doc = None, regex = None, which_kind_of_doccano = None ):
-        """
+    # def load(DoccanoDocument):
+    #     return Document
+    #
+    # def save(Docment, type=None, attribute= None, value=None, span=None, dict_regex_type = None, path_to_pb_doc = None, regex = None, which_kind_of_doccano = None ):
+    #     """
+    #
+    #     :param type:
+    #     :param attribute:
+    #     :param value:
+    #     :param span:
+    #     :param which_kind_of_doccano:
+    #     :return:
+    #     """
+    #     if which_kind_of_doccano == "ima_precision" :
+    #         dict_doccano = toDoccanoImaPrecision(Document, type=type, attribute=attribute)
+    #
+    #     elif which_kind_of_doccano == "ima_rappel":
+    #         dict_doccano = toDoccanoImaRappel(Document, dict_regex_type=dict_regex_type, value=value, span=span)
+    #
+    #     elif which_kind_of_doccano == "ima_pb":
+    #         dict_doccano = toDoccanoPb(Document, path_to_doc=path_to_pb_doc, regexp = regex)
+    #
+    #     else :
+    #         print("Please enter the doccano eval you which to do.")
+    #
+    #     DoccanoDocument = docForDoccano(dict_doccano)
+    #
+    #     return DoccanoDocument
 
-        :param type:
-        :param attribute:
-        :param value:
-        :param span:
-        :param which_kind_of_doccano:
-        :return:
-        """
-        if which_kind_of_doccano == "ima_precision" :
-            dict_doccano = toDoccanoImaPrecision(Document, type=type, attribute=attribute)
-
-        elif which_kind_of_doccano == "ima_rappel":
-            dict_doccano = toDoccanoImaRappel(Document, dict_regex_type=dict_regex_type, value=value, span=span)
-
-        elif which_kind_of_doccano == "ima_pb":
-            dict_doccano = toDoccanoPb(Document, path_to_doc=path_to_pb_doc, regexp = regex)
-
-        else :
-            print("Please enter the doccano eval you which to do.")
-
-        DoccanoDocument = docForDoccano(dict_doccano)
-
-        return DoccanoDocument
 
 
     def toDoccanoImaPrecision(Document, type, attribute=None):
@@ -144,7 +146,7 @@ class DoccanoTransform(DataTransform, Document, DoccanoDocument):
         for text in dict_doccano:
             labels = [dict_doccano[text]]
             meta = {"pymedext_name": dict_doccano[text][0]}
-            thisDoccanoDoc.annotationsDoccano.append(DoccanoAnnotation(text=text,
+            thisDoccanoDoc.doccanoAnnotation.append(DoccanoAnnotation(text=text,
                                                                        labels=labels,
                                                                        meta=meta))
         return thisDoccanoDoc
@@ -161,7 +163,7 @@ class DoccanoTransform(DataTransform, Document, DoccanoDocument):
         for text in dict_doccano:
             labels = dict_doccano[text]
             meta = {"pymedext_name": path_to_doc}
-            DoccanoDocument.append(DoccanoAnnotation(text=text,
+            DoccanoDocument.doccanoAnnotation.append(DoccanoAnnotation(text=text,
                                                      labels=labels,
                                                      meta=meta
                                                              ))
@@ -182,12 +184,12 @@ class DoccanoTransform(DataTransform, Document, DoccanoDocument):
                 number_annoted += 1
                 path_to_doc = os.path.abspath(path_to_doc)
                 # print("inside DoccanoEvalN", number_annoted)
-                DoccanoDocument.append(DoccanoAnnotation(text=text,
+                DoccanoDocument.doccanoAnnotation.append(DoccanoAnnotation(text=text,
                                                                  labels=["x" + dict_doccano[text]],
                                                                  meta={"pymedext_name": path_to_doc}))
         return [DoccanoDocument, number_annoted]
 
-    def DoccanoEvalClass(DoccanoDocument, dict_doccano, dictClasses, number_eval):
+    def DoccanoEvalClass(DoccanoDocument, dict_doccano, dictClasses, number_eval, path_to_doc):
         """
         Adds doccano annotations to DoccanoDocument object until a specified number of evaluations for both classes.
         :param dict_doccano: A doccano dict that will be filled until the two classes reach the desired number of annotations
@@ -197,15 +199,19 @@ class DoccanoTransform(DataTransform, Document, DoccanoDocument):
         """
 
         for text in dict_doccano:
+            path_to_doc = os.path.abspath(path_to_doc)
 
             if dict_doccano[text] not in dictClasses:  # on ne sait pas quelles sont les étiquettes
                 dictClasses[dict_doccano[text]] = 1
-                DoccanoDocument.annotationsDoccano.append(DoccanoAnnotation(text=text,
-                                                                 labels=[dict_doccano[text]]))
+                DoccanoDocument.doccanoAnnotation.append(DoccanoAnnotation(text=text,
+                                                                 labels=[dict_doccano[text]],
+                                                                           meta={"pymedext_name": path_to_doc}))
             elif dictClasses[
                 dict_doccano[text]] < number_eval:  # si l'étiquette n'a pas le nombre désiré d'évaluation, on l'ajoute
                 dictClasses[dict_doccano[text]] += 1
-                DoccanoDocument.annotationsDoccano.append(DoccanoAnnotation(text=text,
-                                                                 labels=[dict_doccano[text]]))
+
+                DoccanoDocument.doccanoAnnotation.append(DoccanoAnnotation(text=text,
+                                                                 labels=[dict_doccano[text]],
+                                                                            meta={"pymedext_name": path_to_doc}))
 
         return [DoccanoDocument, dictClasses]
