e1e754fd43b7f82dc7f14ccadf0a51ba75cc7bf9
willdgn <william.digan@aphp.fr>
2020-08-12 15:25:33 +0200

    delete merge tracks

--- pymedext_core/pymedext.py
+++ pymedext_core/pymedext.py
@@ -6,12 +6,9 @@ from .normalize import *
 from .omopload import *
 from .connector import *
 from .omopsource import *
-<<<<<<< HEAD
 
 from .doccanodocument import DoccanoDocument
 from .doccanotransform import Doccano
 from .doccanoannotator import DoccanoAnnotation
 from .doccanosource import DoccanoSource
-=======
 from .brattransform import brat
->>>>>>> origin/brat
