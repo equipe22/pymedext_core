f814e7b6d4575a031c073a785226c3120971ac06
willdgn <william.digan@aphp.fr>
2020-08-12 16:05:53 +0200

    udpate and change save to brat function

--- pymedext_core/brattransform.py
+++ pymedext_core/brattransform.py
@@ -13,33 +13,32 @@ import logging
 logger = logging.getLogger(__name__)
 
 class brat(DataTransform):
-   
-    def createBratFile(dic_pymedext, bratFilePath_ann):
+    def save(dic_pymedext, bratFilePath_ann):
         f_brat = open(bratFilePath_ann, 'w')
         instance_brat = 0
-        for element in dic_pymedext['annotations']:
-            bratline = 'T' + str(instance_brat) + '	' + dic_pymedext['annotations']['type'] + ' ' + str(dic_pymedext['annotations']['span'][0]) \
-                       + ' ' + str(dic_pymedext['annotations']['span'][0]) + '	' + str(dic_pymedext['annotations']['value'])
+        for annotation in dic_pymedext.annotations:
+            bratline = 'T' + str(instance_brat) + '	' + annotation.type + ' ' + str(annotation.span[0]) \
+                       + ' ' + str(annotation.span[1]) + '	' + str(annotation.value)
             instance_brat += 1
             f_brat.write(bratline)
             f_brat.write('\n')
         f_brat.close()
 
-    def updateBratFile(dic_pymedext, bratFilePath_ann):
-        f_brat = open(bratFilePath_ann, 'r')
-        lastline = ''
-        for line in f_brat:
-            lastline = line
-        f_brat.close()
-        try:
-            instance_brat = int(lastline.split('   ')[0][1:])
-            f_brat = open(bratFilePath_ann, 'a')
-            for element in dic_pymedext['annotations']:
-                bratline = 'T' + str(instance_brat) + '	' + dic_pymedext['annotations']['type'] + ' ' + str(dic_pymedext['annotations']['span'][0]) \
-                           + ' ' + str(dic_pymedext['annotations']['span'][0]) + '	' + str(dic_pymedext['annotations']['value'])
-                instance_brat += 1
-                f_brat.write(bratline)
-                f_brat.write('\n')
-            f_brat.close()
-        except:
-            logger.info('cannot turn into int the value : ' + str(lastline.split('   ')[0]))
+    # def update(dic_pymedext, bratFilePath_ann):
+    #     f_brat = open(bratFilePath_ann, 'r')
+    #     lastline = ''
+    #     for line in f_brat:
+    #         lastline = line
+    #     f_brat.close()
+    #     try:
+    #         instance_brat = int(lastline.split('   ')[0][1:])
+    #         f_brat = open(bratFilePath_ann, 'a')
+    #         for element in dic_pymedext['annotations']:
+    #             bratline = 'T' + str(instance_brat) + '	' + dic_pymedext['annotations']['type'] + ' ' + str(dic_pymedext['annotations']['span'][0]) \
+    #                        + ' ' + str(dic_pymedext['annotations']['span'][0]) + '	' + str(dic_pymedext['annotations']['value'])
+    #             instance_brat += 1
+    #             f_brat.write(bratline)
+    #             f_brat.write('\n')
+    #         f_brat.close()
+    #     except:
+    #         logger.info('cannot turn into int the value : ' + str(lastline.split('   ')[0]))
