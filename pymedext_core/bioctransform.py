9979bbd6f69f0fb48b213da596ecb813ec0c59a0
willdgn <william.digan@aphp.fr>
2020-08-12 14:48:28 +0200

    return now a collection of documents

--- pymedext_core/bioctransform.py
+++ pymedext_core/bioctransform.py
@@ -19,45 +19,48 @@ class BioC(DataTransform):
         raw_text = ""
         raw_text_ID=str(uuid.uuid1())
         # document source = collection.source
-        for passage in collection.documents[0].passages:
-            raw_text = raw_text + passage.text
-            passageID= str(uuid.uuid1())
-            passageAttribute = {value:passage.infons[value]
-                          for value in passage.infons  if value not in ["section_type"] }
-            annotations_list.append(
-                Annotation(type=passage.infons["section_type"],
-                                      value=raw_text,
-                                      ngram = passage.text,
-                                      source_ID=raw_text_ID,
-                                      ID=passageID,
-                                      source="BioCPassage",
-                                      span=(passage.offset,passage.offset+len(passage.text)))
-                )
-            if passage.annotations:
-                for thisAnnotation in passage.annotations:
-                    annotationID= str(uuid.uuid1())
-                    identifier =None
-                    if "identifier" in thisAnnotation.infons.keys() :
-                        identifier=thisAnnotation.infons["identifier"]
-                    else:
-                        identifier=thisAnnotation.infons["Identifier"]
-                    thisAttributes = {value:thisAnnotation.infons[value]
-                                      for value in thisAnnotation.infons  if value not in ["type","identifier","Identifier"] }
-                    annotations_list.append(
-                       Annotation(type=thisAnnotation.infons["type"],
-                                              value=identifier,
-                                              ngram =thisAnnotation.text,
-                                              source_ID=passageID,
-                                              ID=annotationID,
-                                              source="BioCAnnotation",
-                                              span=(thisAnnotation.locations[0].offset,thisAnnotation.locations[0].offset+ thisAnnotation.locations[0].length))
-                        )
-        thisDocument = Document(raw_text =raw_text,ID =raw_text_ID, source = collection.source, documentDate = collection.date)
-        # attributes=collection.key,collection.standalone,
-        # collection.encoding,collection.version
-        # collection.infons
-        thisDocument.annotations.extend(annotations_list)
-        return(thisDocument)
+        documents_collection =[]
+        for doc in collection.documents:
+            for passage in doc.passages:
+                raw_text = raw_text + passage.text
+                passageID= str(uuid.uuid1())
+                passageAttribute = {value:passage.infons[value]
+                              for value in passage.infons  if value not in ["section_type"] }
+                annotations_list.append(
+                    Annotation(type=passage.infons["section_type"],
+                                          value=raw_text,
+                                          ngram = passage.text,
+                                          source_ID=raw_text_ID,
+                                          ID=passageID,
+                                          source="BioCPassage",
+                                          span=(passage.offset,passage.offset+len(passage.text)))
+                    )
+                if passage.annotations:
+                    for thisAnnotation in passage.annotations:
+                        annotationID= str(uuid.uuid1())
+                        identifier =None
+                        if "identifier" in thisAnnotation.infons.keys() :
+                            identifier=thisAnnotation.infons["identifier"]
+                        else:
+                            identifier=thisAnnotation.infons["Identifier"]
+                        thisAttributes = {value:thisAnnotation.infons[value]
+                                          for value in thisAnnotation.infons  if value not in ["type","identifier","Identifier"] }
+                        annotations_list.append(
+                           Annotation(type=thisAnnotation.infons["type"],
+                                                  value=identifier,
+                                                  ngram =thisAnnotation.text,
+                                                  source_ID=passageID,
+                                                  ID=annotationID,
+                                                  source="BioCAnnotation",
+                                                  span=(thisAnnotation.locations[0].offset,thisAnnotation.locations[0].offset+ thisAnnotation.locations[0].length))
+                            )
+            thisDocument = Document(raw_text =raw_text,ID =raw_text_ID, source = collection.source, documentDate = collection.date)
+            # attributes=collection.key,collection.standalone,
+            # collection.encoding,collection.version
+            # collection.infons
+            thisDocument.annotations.extend(annotations_list)
+            documents_collection.append(thisDocument)
+        return(documents_collection)
 
     def __load_collection_xml(bioc_xml):
         with open(bioc_xml, 'r') as fp:
