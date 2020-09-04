9852eada7e33f0b86ff3dc01ee20f32162610709
willdgn <william.digan@aphp.fr>
2020-09-04 17:40:56 +0200

    add brat_parser to read brat file

--- pymedext_core/brattransform.py
+++ pymedext_core/brattransform.py
@@ -11,7 +11,9 @@ fonction : creation ou update  d'un fichier BRAT a partir d'un dic pymedext
 from .datatransform import DataTransform
 import logging
 logger = logging.getLogger(__name__)
-
+import uuid
+from .brat_parser import read_file_annotations
+from .annotators import Annotation
 class brat(DataTransform):
     def savetobrat(dic_pymedext, bratFilePath_ann, exclusion=["raw_text"]):
         f_brat = open(bratFilePath_ann, 'w')
@@ -24,6 +26,30 @@ class brat(DataTransform):
                 f_brat.write(bratline)
                 f_brat.write('\n')
         f_brat.close()
+
+    def load_from_brat(ann_file):
+        entities, relations, attributes, groups=read_file_annotations(ann_file)
+        annotations_list=[]
+        raw_text = open(ann_file.replace(".ann",".txt"), 'r').read()
+        raw_text_ID=str(uuid.uuid1())
+        for entity in entities:
+            for span in entity.span:
+                thisID = str(uuid.uuid1())
+                annotations_list.append(
+                            Annotation(type=entity.type,
+                                      value=entity.text,
+                                      ngram = entity.text,
+                                      source_ID=raw_text_ID,
+                                      ID=thisID,
+                                      source="BratFile",
+                                      span=(span[0],span[1])
+                            )
+        thisDocument = Document(raw_text =raw_text,ID =raw_text_ID, source = ann_file)
+        thisDocument.annotations.extend(annotations_list)
+        return(thisDocument)
+
+
+
 
     # def update(dic_pymedext, bratFilePath_ann):
     #     f_brat = open(bratFilePath_ann, 'r')
